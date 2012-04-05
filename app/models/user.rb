10c52cb4e4dcdfdba4e47df91042aab552c995a1
huseyinozcan <huseyinozcan@gmail.com>
2012-04-05 13:56:42 +0300

    token auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,19 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :authentication_token
   
   has_many :questions
   has_many :answers
+  
+  def ensure_authentication_token!   
+    reset_authentication_token! if authentication_token.blank?   
+  end 
+
+  before_save :ensure_authentication_token!  
   
    #authorizations
   ROLES = %w[admin moderator author banned]
