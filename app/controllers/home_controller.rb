13322238adfb64fb54ebaf33182d5523854eea86
huseyinozcan <huseyinozcan@gmail.com>
2012-04-07 02:14:34 +0300

    nopicture count on button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
       @question_count= Question.count
       @answer_count= Answer.count
       @user_count= User.count
+      @nopicture_count=Question.nopicture.count
       
       sql="select count(*) cnt, u.email,q.user_id from users u, questions q where u.id=q.user_id group by q.user_id"
       @user_questions = Question.find_by_sql([sql])
