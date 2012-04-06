bcc5454cf260c07f1fc942de086dc42ed7c0b8d4
huseyinozcan <huseyinozcan@gmail.com>
2012-04-06 18:08:37 +0300

    prevent registration from web

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -15,6 +15,9 @@ class RegistrationsController < Devise::RegistrationsController
       }
     end
    end
- 
-
+   
+  def new
+    flash[:info] = 'Registrations are not open yet, but please check back soon'
+    redirect_to user_session_path
+  end
 end
