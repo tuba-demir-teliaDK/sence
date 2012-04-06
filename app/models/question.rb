39fb89facb7f90a5ba74c48a4fd55ba447339b52
huseyinozcan <huseyinozcan@gmail.com>
2012-04-06 19:11:29 +0300

    capitalize disable

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :user
   
-  before_save :capitalize_fields
+  #before_save :capitalize_fields
   has_attached_file :opt1_image, :styles => { :medium => "180x180>", :thumb => "32x32>" }
   has_attached_file :opt2_image, :styles => { :medium => "180x180>", :thumb => "32x32>" }
   
