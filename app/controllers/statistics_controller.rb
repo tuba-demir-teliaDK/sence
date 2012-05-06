d5a84b549b890111c58366cf11fe49f45bd49ea0
huseyinozcan <huseyinozcan@gmail.com>
2012-05-06 20:25:40 +0300

    aomq

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,8 +1,7 @@
 class StatisticsController < ApplicationController
   # GET /questions
   # GET /questions.json
-  skip_authorize_resource :only => :random
-  
+  authorize_resource :class => false  
   
   def most_answered
     sql="select * from (select opt1_ac+opt2_ac tc,id,opt1,opt2,opt1_ac,opt2_ac from questions) x order by tc desc"
@@ -23,6 +22,17 @@ class StatisticsController < ApplicationController
       format.html # show.html.erb
       format.json { render json: @gap.to_json(:except=>[:tc]) }
     end  
+  end
+  
+  def aomq
+    @user = User.find(current_user)
+    sql="select * from questions where user_id=? order by created_at desc"
+    
+    @aomq=Question.find_by_sql([sql,@user.id])
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @aomq.to_json(:only=>[:id,:opt1,:opt2,:opt1_ac,:opt2_ac,:status,:opt1_image_file_name,:opt2_image_file_name]) }
+    end
   end
   
 end
