d79fe85862a8579e161a2363a36c18ddd4dc1fe8
huseyinozcan <huseyinozcan@gmail.com>
2012-05-08 10:16:12 +0300

    aynı device_id geldiginde auth_token döner

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,16 +1,26 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def create
+    user= User.find_by_apple_id(params["user"]["apple_id"])
+    paramemail=params["user"]["email"]
+    paramemail_by_appleid = params["user"]["apple_id"].to_s + "@igones.com"
+    
     respond_to do |format|  
       format.html { 
         super 
       }
       format.json {
         build_resource
-        if resource.save
-           render :status => 200, :json => resource.to_json(:only=> :authentication_token)
+        if user.nil?
+          if resource.save
+             render :status => 200, :json => resource.to_json(:only=> :authentication_token)
+          else
+            render :json => resource.errors, :status => :unprocessable_entity
+          end
+        elsif !user.nil? and user.email==paramemail and paramemail==paramemail_by_appleid
+            render :status => 200, :json => user.to_json(:only=> :authentication_token)
         else
-          render :json => resource.errors, :status => :unprocessable_entity
+            render :json => "server error", :status => :unprocessable_entity
         end
       }
     end
